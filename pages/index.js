996cf4047bb06840fc1263c6ff6c468e7bafc552
Jumi Pham <npham32@my.bcit.ca>
2024-03-25 14:44:31 -0700

    ParentTwo changes

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
 import ParentToChild from "./ParentOne";
-import ChildToParent from "./parentTwo";
+import ChildToParent from "./ParentTwo";
 
 
 export default function Home() {
